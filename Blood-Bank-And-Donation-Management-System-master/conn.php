4979a70ebfdbfb84979d05cd52440d0a9bbc9329
Ayush Mahant <71230356+Ayushmahant@users.noreply.github.com>
2025-04-08 23:45:11 +0530

    Update conn.php

--- Blood-Bank-And-Donation-Management-System-master/conn.php
+++ Blood-Bank-And-Donation-Management-System-master/conn.php
@@ -1,3 +1,12 @@
 <?php
-$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
+$host = getenv("DB_HOST") ?: "hopper.proxy.rlwy.net";
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "UJRlQnzkfqEvNeFggHjYEHMhhfnKSeCQ";
+$db   = getenv("DB_NAME") ?: "railway";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
