cf76d5bf714341162dc30c53f0504860444c30e2
Ayush Mahant <71230356+Ayushmahant@users.noreply.github.com>
2025-04-16 00:28:19 +0530

    Update conn.php

--- Blood-Bank-And-Donation-Management-System-master/conn.php
+++ Blood-Bank-And-Donation-Management-System-master/conn.php
@@ -10,5 +10,4 @@ $conn = mysqli_connect($host, $username, $password, $database, $port);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "✅ Connected successfully to Railway MySQL!";
 ?>
