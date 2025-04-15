5e8650762fd4dc2e5af6b953eb0bde843d6c3148
Ayush Mahant <71230356+Ayushmahant@users.noreply.github.com>
2025-04-16 00:23:09 +0530

    Update savedata.php

--- Blood-Bank-And-Donation-Management-System-master/savedata.php
+++ Blood-Bank-And-Donation-Management-System-master/savedata.php
@@ -6,7 +6,7 @@ $age=$_POST['age'];
 $gender=$_POST['gender'];
 $blood_group=$_POST['blood'];
 $address=$_POST['address'];
-$conn=mysqli_connect('hopper.proxy.rlwy.net', 'root', 'UJRlQnzkfqEvNeFggHjYEHMhhfnKSeCQ', 'railway', 50657)
+$conn=mysqli_connect('hopper.proxy.rlwy.net', 'root', 'UJRlQnzkfqEvNeFggHjYEHMhhfnKSeCQ', 'railway', 50657);
 $sql= "INSERT INTO donor_details(donor_name,donor_number,donor_mail,donor_age,donor_gender,donor_blood,donor_address) values('{$name}','{$number}','{$email}','{$age}','{$gender}','{$blood_group}','{$address}')";
 $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
 
